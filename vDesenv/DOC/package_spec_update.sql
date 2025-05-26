44e1aaca6eea42aeaa5064d6e579a649b1e19c69
ivanor-upquery <ivanor@uquery.com>
2025-05-26 09:49:25 -0300

    excluido arquivo .sql

--- vDesenv/DOC/package_spec_update.sql
+++
@@ -1,7 +0,0 @@
--- Execute this script to update the DOC package specification with the new procedure
-
-SET DEFINE OFF
-
-ALTER PACKAGE DOC ADD
-procedure conteudo_excluir (prm_id_conteudo varchar2);
-/
